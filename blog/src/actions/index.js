8f30c831073e56bfa19eaa03d39ab6abdb869c35
Itumeleng Mochabe <itumelengmochabe@gmail.com>
2019-09-19 10:36:11 +0200

    Commiting the changes I made since yesterday, app does not work. Need to add middleware

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -3,6 +3,7 @@ import jsoPlaceholder from "../apis/jsonPlaceholder";
 export const fetchPosts = async () => {
 	const response = await jsoPlaceholder.get("./posts");
 	return {
-		type: "FETCH_POSTS"
+		type: "FETCH_POSTS",
+		payload: response
 	};
 };
