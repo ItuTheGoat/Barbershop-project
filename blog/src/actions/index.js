a76c108053269513aff3c9c7d7a5599eb7f5d690
Itumeleng Mochabe <itumelengmochabe@gmail.com>
2019-09-23 11:46:58 +0200

    A bunch of refactoring in order to make the user requests more efficient and make the action creators more readable

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -7,11 +7,32 @@ export const fetchPosts = () => async dispatch => {
 	dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-export const fetchUsers = id => dispatch => _fetchUser(id, dispatch);
-
-// memoize the Interior function which fetches each user exactly once.
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUsers = id => async dispatch => {
 	const response = await jsoPlaceholder.get(`./users/${id}`);
 
 	dispatch({ type: "FETCH_USERS", payload: response.data });
-});
+};
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+	//Need to dispatch the original action creators when nesting action creators
+	// the await keyword will ensure we wait for the api request to get handled
+	await dispatch(fetchPosts());
+
+	// const userIds = _.uniq(_.map(getState().posts, "userId"));
+	// userIds.forEach(id => dispatch(fetchUsers(id)));
+
+	// Lodash chain method to simplify
+	_.chain(getState().posts)
+		.map("userId")
+		.uniq()
+		.forEach(id => dispatch(fetchUsers(id)))
+		.value();
+};
+// memoize the Interior function which fetches each user exactly once.
+// export const fetchUsers = id => dispatch => _fetchUser(id, dispatch);
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+// 	const response = await jsoPlaceholder.get(`./users/${id}`);
+
+// 	dispatch({ type: "FETCH_USERS", payload: response.data });
+// });
