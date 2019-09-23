50ad820b10fb8a55d001214d7f260bf660c9dc2f
Itumeleng Mochabe <itumelengmochabe@gmail.com>
2019-09-23 11:05:13 +0200

    Memoized the hell outta the fetch user action creator

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import jsoPlaceholder from "../apis/jsonPlaceholder";
 // This is how we use redux-thunk. We use it  with the async await function.
 export const fetchPosts = () => async dispatch => {
@@ -6,8 +7,11 @@ export const fetchPosts = () => async dispatch => {
 	dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-export const fetchUsers = id => async dispatch => {
+export const fetchUsers = id => dispatch => _fetchUser(id, dispatch);
+
+// memoize the Interior function which fetches each user exactly once.
+const _fetchUser = _.memoize(async (id, dispatch) => {
 	const response = await jsoPlaceholder.get(`./users/${id}`);
 
 	dispatch({ type: "FETCH_USERS", payload: response.data });
-};
+});
