c90261cbcc97cded57907e32da30ee695037a483
Itumeleng Mochabe <itumelengmochabe@gmail.com>
2019-11-04 13:47:11 +0200

    Initial commit for barbershop project

---
+++ barberproject/src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { BrowserRouter as Router } from "react-router-dom";
+
+class App extends React.Component {
+	render() {
+		return (
+			<Router>
+				<div></div>
+			</Router>
+		);
+	}
+}
+
+export default App;
