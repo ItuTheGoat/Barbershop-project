99378d3b9227870b384da0d1ca71f3a0a8e837b8
kade-shikaal <skadebis@gmail.com>
2019-12-04 17:30:49 +0200

    Updates

---
+++ precache-manifest.1d1a2d33dc4ebd1751b2c3bbbd58385e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "244817ab61306f44c1c4315c866f2076",
+    "url": "/Barbershop-project/barberproject/index.html"
+  },
+  {
+    "revision": "00fbb0a7b16d22f3bf06",
+    "url": "/Barbershop-project/barberproject/static/css/main.fc104ec9.chunk.css"
+  },
+  {
+    "revision": "53e834f236551526ad55",
+    "url": "/Barbershop-project/barberproject/static/js/2.b98a3c6f.chunk.js"
+  },
+  {
+    "revision": "00fbb0a7b16d22f3bf06",
+    "url": "/Barbershop-project/barberproject/static/js/main.5e54b7c1.chunk.js"
+  },
+  {
+    "revision": "6d1803dd16f2eef4aff8",
+    "url": "/Barbershop-project/barberproject/static/js/runtime-main.6328f61b.js"
+  },
+  {
+    "revision": "61fb7a129f00c481cecd90e16733ca63",
+    "url": "/Barbershop-project/barberproject/static/media/barber.61fb7a12.png"
+  },
+  {
+    "revision": "36d70bafec41bcb9e87e55999bdaae20",
+    "url": "/Barbershop-project/barberproject/static/media/selfie.36d70baf.png"
+  }
+]);
