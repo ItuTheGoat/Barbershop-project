73d59247475baff80d5c9505068f788d6e5cf766
kade-shikaal <skadebis@gmail.com>
2019-12-10 16:56:24 +0200

    edited signup

--- barberproject/src/components/signUp.js
+++ barberproject/src/components/signUp.js
@@ -1,23 +1,45 @@
 import React, { Component } from "react";
+import "./signUp.css";
 
 // I think we will need state here.
 class SignUp extends Component {
-	render() {
-		return (
-			<div className='container'>
-				<form>
-					<div className='form-group row justify-content-md-center'>
-						<label htmlFor='userEmail'>Email: </label>
-						<input type='email' id='userEmail' className='form-control' />
-					</div>
-					<div className='form-group'>
-						<label htmlFor='userPassword'>Password: </label>
-						<input type='password' id='userPassword' className='form-control' />
-					</div>
-				</form>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="container">
+        <div className="row cont-body">
+          <div className="col-lg-5">
+            <div className="card">
+              <div className="card-body">
+                <form className="text-center ">
+                  <label htmlFor="userEmail">Email: </label>
+                  <input
+                    type="email"
+                    id="userEmail"
+                    className="form-control mb-4"
+                    placeholder="Email"
+                  />
+                  <label htmlFor="userPassword">Password: </label>
+                  <input
+                    type="password"
+                    id="userPassword"
+                    className="form-control mb-4"
+                    placeholder="Password"
+                  />
+                  <button class="btn btn-info btn-block my-4" type="submit">
+                    Sign up
+                  </button>
+                  <div>Already have an account?</div>
+                  <a href="/" target="_blank">
+                    Sign In
+                  </a>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default SignUp;
